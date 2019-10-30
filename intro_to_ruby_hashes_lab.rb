16a384f22898dca858ba4a6451e2130c0c192b6e
Juan Gonzalez <3232rose@gmail.com>
2019-10-30 16:30:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly_with_third_tier
   :pennsylvania_railroads => {},
   :b_and_o_railroads => {}, 
   :shortline_railroads => {},
-  :rent_in_dollars => {:one_pieces
+  :rent_in_dollars => {:one_piece
 end
 
 def monopoly_with_fourth_tier
