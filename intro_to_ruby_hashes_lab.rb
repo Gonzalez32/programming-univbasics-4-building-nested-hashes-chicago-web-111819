3a621a2b445590053d0de8e08d4d56f99d739f1d
Juan Gonzalez <3232rose@gmail.com>
2019-10-30 16:25:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {:railroads => {:names => {:reading_railroads => 
+  {:railroads => {:names => {:reading_railroads =>, 
 end
 
 def monopoly_with_fourth_tier
