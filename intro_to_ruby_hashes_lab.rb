f2fe6dae870589a7297964d44dfe809818166f6d
Juan Gonzalez <3232rose@gmail.com>
2019-10-30 16:53:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_fourth_tier
   :shortline_railroads => {"mortgage_value" => "$800"} },
     
     
-  :rent_in_dollars => {
+   :rent_in_dollars => {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
